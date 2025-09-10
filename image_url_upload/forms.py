a4150be5734aefdd8c4dec8166a3701dfb81163d
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-09-10 17:05:51 +0500

    Update forms.py

--- image_url_upload/forms.py
+++ image_url_upload/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
+from django.core.exceptions import ValidationError
+import requests
 
 class AddImageFromURLForm(forms.Form):
     image_url = forms.URLField(
