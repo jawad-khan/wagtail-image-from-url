d57e81ed4ddc2e03c8471d0987b0589f662a19a3
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-09-10 16:16:41 +0500

    Update views.py

--- image_url_upload/views.py
+++ image_url_upload/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from wagtail.admin import messages as wagtail_messages
+from wagtail.admin.ui.components import BreadcrumbItem
+from django.utils.translation import gettext_lazy as _
 
 from .forms import ImageUrlForm
-from .utils import get_image_from_url   # ✅ import the util function
+from .utils import get_image_from_url
 
 
 @staff_member_required
 def add_image_via_url(request):
-     breadcrumbs_items = [
+    breadcrumbs_items = [
         BreadcrumbItem(_('Images'), url='wagtailimages:index'),
         BreadcrumbItem(_('Add image from URL')),
     ]
-
 
     if request.method == "POST":
         form = ImageUrlForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data["image_url"]
             try:
-                # ✅ actually save the image here
                 image = get_image_from_url(url, user=request.user)
-
                 wagtail_messages.success(
                     request, f"Image '{image.title}' added successfully."
                 )
@@ -32,4 +31,8 @@ def add_image_via_url(request):
     else:
         form = ImageUrlForm()
 
-    return render(request, "image_url_upload/add_via_url.html", {"form": form, "breadcrumbs_items": breadcrumbs_items})
+    return render(
+        request,
+        "image_url_upload/add_via_url.html",
+        {"form": form, "breadcrumbs_items": breadcrumbs_items},
+    )
