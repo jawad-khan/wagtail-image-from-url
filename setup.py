45c9aee8d30f614e1d3ebf02b99f893669235e17
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-09-10 15:51:18 +0500

    Enhance setup.py with additional metadata
    Added classifiers and python_requires to setup.py.

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="image-url-upload",
     version="0.1.0",
+    description="Add images to Wagtail via URL",
+    author="Awais Qureshi",
     packages=find_packages(include=["image_url_upload", "image_url_upload.*"]),
-    include_package_data=True,
+    include_package_data=True,  # ✅ includes templates and static files
     install_requires=[
         "Django>=4.2",
         "Wagtail>=5.0",
         "requests",
     ],
-    url="https://github.com/awais786/wagtail-image-from-url",
-    license="MIT",
-    author="Awais Qureshi",
-    description="Add images to Wagtail via URL",
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Wagtail",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
