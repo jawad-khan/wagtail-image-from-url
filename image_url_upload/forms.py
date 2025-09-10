ea3f24c7948f8b55938b48567bc871d50d9b515a
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-09-10 16:46:36 +0500

    Update forms.py

--- image_url_upload/forms.py
+++ image_url_upload/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
-class ImageUrlForm(forms.Form):
+
+class AddImageFromURLForm(forms.Form):
     image_url = forms.URLField(
-        label="Image URL",
+        label=_("Image URL"),
         required=True,
-        widget=forms.URLInput(attrs={"placeholder": "Paste image URL here", "class": "form-control"}),
+        widget=forms.URLInput(attrs={
+            "placeholder": _("https://example.com/image.jpg"),
+            "class": "w-input"
+        }),
+        help_text=_("Enter a direct link to an image.")
     )
