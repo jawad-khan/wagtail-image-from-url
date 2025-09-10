a67055795e56b555039f2d63823f82c0cd4049d3
Awais Qureshi <awais.qureshi@arbisoft.com>
2025-09-10 17:37:34 +0500

    Update views.py

--- image_url_upload/views.py
+++ image_url_upload/views.py
@@ -1,19 +1,25 @@
 # image_url_upload/views.py
-
 import requests
 from django import forms
 from django.core.files.base import ContentFile
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 from wagtail.admin.views import generic
 from wagtail.images import get_image_model
 
+
 MAX_FILE_SIZE_MB = 5
+ImageModel = get_image_model()
 
 
-class ImageURLForm(forms.Form):
+class ImageURLForm(forms.ModelForm):
     image_url = forms.URLField(label=_("Image URL"))
+
+    class Meta:
+        model = ImageModel
+        fields = []  # we only use our custom URL field
 
     def clean_image_url(self):
         url = self.cleaned_data["image_url"]
@@ -34,27 +40,38 @@ class ImageURLForm(forms.Form):
 
 class AddImageViaURLView(generic.CreateView):
     """
-    A Wagtail admin CreateView to add images via URL.
+    Wagtail admin view for adding images via URL.
     """
 
     form_class = ImageURLForm
     template_name = "image_url_upload/add.html"
     page_title = _("Add image via URL")
     header_icon = "image"
-    model = get_image_model()
+    model = ImageModel
 
     def form_valid(self, form):
         url = form.cleaned_data["image_url"]
         response = requests.get(url, stream=True, timeout=10)
         response.raise_for_status()
 
-        file_name = url.split("/")[-1]
+        file_name = url.split("/")[-1] or "imported.jpg"
         image_file = ContentFile(response.content, name=file_name)
 
-        # create new image instance
         self.object = self.model.objects.create(
             title=file_name,
             file=image_file,
+            uploaded_by_user=self.request.user,
         )
 
         return redirect("wagtailimages:index")
+
+    def get_breadcrumbs(self):
+        """
+        Override breadcrumbs to show:
+        Home → Images → Add image via URL
+        """
+        return [
+            {"url": reverse("wagtailadmin_home"), "label": _("Home")},
+            {"url": reverse("wagtailimages:index"), "label": _("Images")},
+            {"url": "", "label": _("Add image via URL")},
+        ]
